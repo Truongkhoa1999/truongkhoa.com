7a3ee8e10306a6525d52a2d17a909b980e6a7e80
Khoa <tdkhoa.aqua@gmail.com>
2023-10-02 21:05:10 +0300

    siu

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -8,49 +8,63 @@ export const projectsData: ProjectDataProps[] = [
   {
     name: "Thebra.fi",
     role: "Freelance",
-    image: "dist/assets/thumbnail/thebra.avif",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Fthebra.avif?alt=media&token=78cf1ac0-3989-41f9-a231-c9d2f43bec8b&_gl=1*xiba74*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg4NjcuNjAuMC4w",
     link: "https://thebra.fi/",
   },
   {
     name: "Mehtaheino.com",
     role: "Freelance",
-    image: "dist/assets/thumbnail/MH.avif",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2FMH.avif?alt=media&token=39f7a680-4478-4ef9-b9d6-4b18ca3059af&_gl=1*1pi7yu1*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg3ODAuNy4wLjA.",
     link: "https://mehtaheino.com/",
+  },
+  { 
+    name: "Portfolio",
+    role: "version 1.0",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Fportfolio1.avif?alt=media&token=67f13c44-72e4-41a5-9781-d84d7b4c90dc&_gl=1*1sz6x4d*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjk3MjMuNjAuMC4w",
+    link: "https://github.com/Truongkhoa1999/fs14-fullstack",
   },
   {
     name: "Fullstack Ecommerce",
     role: "Elisa's challenge (sever shutdown) - 4.5 GPA",
-    image: "dist/assets/thumbnail/GH.avif",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2FGH.avif?alt=media&token=503e4fb3-0fbc-4bb6-9ef0-a1d19d276f3e&_gl=1*11ki2bw*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg3NDAuNDcuMC4w",
     link: "https://github.com/Truongkhoa1999/fs14-fullstack",
   },
   {
     name: "Fine Dinin'",
     role: "Resposnsive assignment",
-    image: "dist/assets/thumbnail/Finedin.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Ffinedining.avif?alt=media&token=9cf76d38-30ce-4b65-9955-f40d2009f553&_gl=1*eme5w5*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjkxMTUuNTUuMC4w",
     link: "https://fancy-tiramisu-211459.netlify.app/",
   },
   {
     name: "Scientific Calculator",
     role: "Self-study",
-    image: "dist/assets/thumbnail/SC.avif",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2FSC.avif?alt=media&token=6fe5b71f-3720-4d8d-9228-525594362cdb&_gl=1*tm8uwm*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg4MTYuNDEuMC4w",
     link: "https://reliable-taiyaki-d7ba21.netlify.app/",
   },
   {
     name: "Weather Forecast",
     role: "Inte FE challenge",
-    image: "dist/assets/thumbnail/WF.webp",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Fwc.avif?alt=media&token=5579d6af-5bc6-49a4-84c3-1819b7d7f30d&_gl=1*13ork3*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjk2NTIuNTAuMC4w",
     link: "https://steady-fairy-d67fa1.netlify.app/",
   },
   {
     name: "Todo App",
     role: "Inte BE challenge",
-    image: "dist/assets/thumbnail/todo.avif",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Ftodo.avif?alt=media&token=3dc4a857-5371-4a5e-a887-069422df8f67&_gl=1*12h6upe*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg4ODMuNDQuMC4w",
     link: "https://github.com/Truongkhoa1999/todoApp",
   },
   {
     name: "3D Video",
     role: "Mehtaheino promotion",
     image: "dist/assets/thumbnail/MHV.avif",
-    link: "https://www.youtube.com/watch?v=9_b04gB4x3U&ab_channel=KhoaTr%C6%B0%C6%A1ng%C4%90%C4%83ng",
+    link: "https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2FMHV.avif?alt=media&token=40246e79-5675-4bc9-861d-26262e1bed5e&_gl=1*zklcif*_ga*NDk2MzQ2NTIzLjE2ODk4ODczMDY.*_ga_CW55HF8NVT*MTY5NjI2NzI4Ni4zNC4xLjE2OTYyNjg3OTcuNjAuMC4w",
   },
 ];

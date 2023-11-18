3a9e0f59c4f31aec48282d5bc84846962f585d5f
Khoa Truong <khoatruong@Khoas-iMac.local>
2023-11-19 00:42:39 +0200

    1.0.2

--- src/component/certificate/Certificate.tsx
+++ src/component/certificate/Certificate.tsx
@@ -6,6 +6,7 @@ export const Certificate = () => {
             <div className='item'>
                 <a target='_blank' href="https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/certificate%2Finte.pdf?alt=media&token=23261113-b9af-48f1-aed8-becac17f4f5f&_gl=1*1h6sdp6*_ga*MTUwMTg2NDkwMS4xNjk4NTI2NTcy*_ga_CW55HF8NVT*MTY5ODUzODc4Ny4zLjEuMTY5ODUzODgxMy4zNC4wLjA.">FullStack Trainee Graduation, Integrify, Finland.</a>
                 <a target='_blank' href="https://www.freecodecamp.org/certification/Khoa99/responsive-web-design">Responsive Web Design Certifcation, Freecodecamp</a>
+                <a target='_blank' href="https://www.freecodecamp.org/certification/Khoa99/back-end-development-and-apis">Backend Development and APIs with NodeJs, Freecodecamp</a>
                 <a target='_blank' href="https://www.freecodecamp.org/certification/Khoa99/javascript-algorithms-and-data-structures">JavaScript Algorithms and Data Structures Certifcation, Freecodecamp.</a>
                 <a target='_blank' href="https://www.credly.com/badges/e2af856c-f1c7-4dfd-b00a-6663ae21e20b">AWS Re-start Certification, Amazon Web Services Training and Certification</a>
                 <a target='_blank' href="">Bachelor Degree in Business Information Technology Graduation, Haaga-helia, Finland.</a>             

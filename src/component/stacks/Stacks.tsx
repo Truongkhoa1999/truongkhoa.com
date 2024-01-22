229898be9573aecd6f387b2b0c77b55ac0b4ddea
Khoa <tdkhoa.aqua@gmail.com>
2024-01-22 17:04:29 +0200

    working on projects site

--- src/component/stacks/Stacks.tsx
+++ src/component/stacks/Stacks.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 import "./style/stacks.scss";
 // import { handleSwitchStack } from '../../utils/handleSwitchStack'
-import { FrontendStack } from "./FrontendStack";
-import { BackendStack } from "./BackendStack";
-import { DatabaseStack } from "./DatabaseStack";
-import { CloudStack } from "./CloudStack";
-import { DevopStack } from "./DevopStack";
+
 import { useParams } from "react-router-dom";
 
 export const Stacks = () => {
@@ -13,34 +9,32 @@ export const Stacks = () => {
   const params = useParams();
   const paramsKey = Object.keys(params)[0];
   let paramsValue = params[paramsKey as keyof typeof params];
-  console.log(params)
+  console.log(params);
   const handleStacks = (paramsValue: string) => {
     const regex = /\/([^/]+)$/;
-    const lastStringStack = paramsValue.match(regex)
+    const lastStringStack = paramsValue.match(regex);
     if (lastStringStack && lastStringStack.length > 1) {
-      setStack(lastStringStack[1])
+      setStack(lastStringStack[1]);
     }
-
-  }
+  };
 
   useEffect(() => {
-    handleStacks(paramsValue!)
-  }, [params])
+    handleStacks(paramsValue!);
+  }, [params]);
 
   return (
-    <div className="stacks">
-      <div className="background"></div>
-      <div className="checkerboard"></div>
-
-      <h1>{stack}</h1>
-      <div>
-        {
-          stack === "Frontend" ? <FrontendStack /> : stack === "Backend" ? <BackendStack /> : stack === "Database" ? <DatabaseStack /> : stack === "Cloud" ? <CloudStack /> : stack === "DevOps" ? <DevopStack /> : ""
-        }
+    <div className="stack__container">
+      {/* <div className="background"></div> */}
+      {/* <div className="checkerboard"></div> */}
+      {/* <h1>{stack}</h1> */}
+      <div className="stack__group">
+        <div className="stack__card"></div>
+        <div className="stack__card"></div>
+        <div className="stack__card"></div>
       </div>
+      <div className="certificate__container">
 
-
-
+      </div>
     </div>
   );
 };

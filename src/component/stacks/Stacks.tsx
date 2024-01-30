60b59d1296aa787b0bec59db7b8fcbcf56c46028
Khoa <tdkhoa.aqua@gmail.com>
2024-01-30 16:11:14 +0200

    stack sites done

--- src/component/stacks/Stacks.tsx
+++ src/component/stacks/Stacks.tsx
@@ -1,40 +1,62 @@
-import { useEffect, useState } from "react";
+import {  useRef, useState } from "react";
 import "./style/stacks.scss";
-// import { handleSwitchStack } from '../../utils/handleSwitchStack'
+import { ExpandedCertificate } from "../certificate/ExpandedCertificate";
 
-import { useParams } from "react-router-dom";
 
 export const Stacks = () => {
-  const [stack, setStack] = useState<string>("Frontend");
-  const params = useParams();
-  const paramsKey = Object.keys(params)[0];
-  let paramsValue = params[paramsKey as keyof typeof params];
-  console.log(params);
-  const handleStacks = (paramsValue: string) => {
-    const regex = /\/([^/]+)$/;
-    const lastStringStack = paramsValue.match(regex);
-    if (lastStringStack && lastStringStack.length > 1) {
-      setStack(lastStringStack[1]);
+const [isExpandedNotificationVisible, setIsExpandedNotificationVisible] = useState<boolean>(false)
+
+  const targetElementRef = useRef<HTMLDivElement>(null);
+  const handleScrollToTarget = () => {
+    if (targetElementRef.current) {
+      targetElementRef.current.scrollIntoView({
+        behavior: "smooth", 
+        block: "start", 
+        inline: "nearest",
+      });
     }
   };
-
-  useEffect(() => {
-    handleStacks(paramsValue!);
-  }, [params]);
-
+  const handleSwitchNotificationStatus = () => {
+    setIsExpandedNotificationVisible(true)
+  }
+  
   return (
     <div className="stack__container">
-      {/* <div className="background"></div> */}
-      {/* <div className="checkerboard"></div> */}
-      {/* <h1>{stack}</h1> */}
-      <div className="stack__group">
-        <div className="stack__card"></div>
-        <div className="stack__card"></div>
-        <div className="stack__card"></div>
+      <div className="stack__group" >
+        <div onClick={handleScrollToTarget} className="stack__card">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Ffe.svg?alt=media&token=ea6cda26-9960-40bd-bba7-bd795c58ef69"
+            alt="1"
+          />
+          <h2>FRONTEND</h2>
+          <p>Typescript, JavaScript, HTML SASS, ReactJS, Redux</p>
+        </div>
+        <div onClick={handleScrollToTarget} className="stack__card">
+          {" "}
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Fbe.svg?alt=media&token=e7d4411e-55f0-473b-99e6-6ef79bd21df4"
+            alt="1"
+          />
+          <h2>BACKEND</h2>
+          <p>Java, C#, C++, PostgreSQL, MongoDB SpringBoot, ExpressJS </p>
+        </div>
+        <div onClick={handleScrollToTarget} className="stack__card">
+          {" "}
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/truongkhoaportfolio.appspot.com/o/thumbnail%2Fside.svg?alt=media&token=2e067cdf-a4de-4849-ae56-c8df357e924e"
+            alt="1"
+          />
+          <h2>SIDE</h2>
+          <p>AWS, Heroku, Netlify, Github, Docker</p>
+        </div>{" "}
       </div>
-      <div className="certificate__container">
-
+      <div ref={targetElementRef} className="certificate__container">
+        <p>It has been such a great tech-journey since 2021</p>
+        <button onClick={handleSwitchNotificationStatus}>CERTIFICATE</button>
       </div>
+      {
+        isExpandedNotificationVisible? (<ExpandedCertificate setIsExpandedCertificate={setIsExpandedNotificationVisible} />) : ""
+      }
     </div>
   );
 };

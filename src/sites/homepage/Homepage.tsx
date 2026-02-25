50ccce60ad44e4e99939ac1545a9d8935eeba1ed
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2026-02-25 12:59:53 +0200

    build fix@

--- src/sites/homepage/Homepage.tsx
+++ src/sites/homepage/Homepage.tsx
@@ -1,38 +1,37 @@
-import { useState } from "react";
 // style
 import "./style/homepage.scss";
 import "../../utils/style/scrollbar.scss";
 
 import { Header } from "../../component/header/Header";
 // import { Footer } from "../../component/footer/Footer";
-interface NavProps {
-  about: boolean;
-  contact: boolean;
-  stacks: boolean;
-  resume: boolean;
-  portfolio: boolean;
-}
+// interface NavProps {
+//   about: boolean;
+//   contact: boolean;
+//   stacks: boolean;
+//   resume: boolean;
+//   portfolio: boolean;
+// }
 
 export const Homepage = () => {
-  const [elementsState, setElementsState] = useState<NavProps>({
-    about: true,
-    stacks: false,
-    resume: false,
-    contact: false,
-    portfolio: false,
-  });
-  const handleElementsAreClicked = (elements: string) => {
-    const updatedObject = { ...elementsState } as NavProps;
+  // const [elementsState, setElementsState] = useState<NavProps>({
+  //   about: true,
+  //   stacks: false,
+  //   resume: false,
+  //   contact: false,
+  //   portfolio: false,
+  // });
+  // const handleElementsAreClicked = (elements: string) => {
+  //   const updatedObject = { ...elementsState } as NavProps;
 
-    for (const key in updatedObject) {
-      if (key === elements) {
-        updatedObject[key as keyof NavProps] = true;
-      } else {
-        updatedObject[key as keyof NavProps] = false;
-      }
-    }
-    setElementsState(updatedObject);
-  };
+  //   for (const key in updatedObject) {
+  //     if (key === elements) {
+  //       updatedObject[key as keyof NavProps] = true;
+  //     } else {
+  //       updatedObject[key as keyof NavProps] = false;
+  //     }
+  //   }
+  //   setElementsState(updatedObject);
+  // };
 
   return (
     <div className="homepage__container">

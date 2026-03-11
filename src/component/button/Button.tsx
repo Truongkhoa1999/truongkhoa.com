96dc32baf82b283a020f6c35c11d532ee9ebf053
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2026-03-11 15:16:55 +0200

    enhance contact button

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -7,33 +7,46 @@ type ButtonProps = {
   startIcon?: IconProp;
   onClick?: () => void;
 };
+const commonStyle = {
+  display: "flex",
+  gap: 5,
+  padding: "8px 16px",
+  borderRadius: 8,
+  border: "1px solid rgba(0,0,0,0.2)",
+  textDecoration: "none",
+  color: "black",
+  fontWeight: 500,
+  fontSize: 14,
+  transition: "all 0.2s ease",
+};
+export function CustomButton({
+  hyperLink,
+  title,
+  startIcon,
+  onClick,
+}: ButtonProps) {
+  if (hyperLink.length > 0) {
+    return (
+      <div style={commonStyle}>
+        {startIcon ? (
+          <FontAwesomeIcon
+            style={{ marginRight: 4 }}
+            icon={startIcon as IconProp}
+          />
+        ) : null}
+        <a href={hyperLink} target="_blank" style={{
+          color:'black'
+        }}>
+          {title}
+        </a>
+      </div>
+    );
+  }
 
-export function CustomButton({ hyperLink, title, startIcon, onClick }: ButtonProps) {
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: 5,
-        padding: "8px 16px",
-        borderRadius: 8,
-        border: "1px solid rgba(0,0,0,0.2)",
-        textDecoration: "none",
-        color: "black",
-        fontWeight: 500,
-        fontSize: 14,
-        transition: "all 0.2s ease",
-      }}
-      onClick={onClick}
-    >
-      {startIcon ? (
-        <FontAwesomeIcon
-          style={{ marginRight: 4 }}
-          icon={startIcon as IconProp}
-        />
-      ) : null}
-      <a href={hyperLink} target="_blank">
-        {title}
-      </a>
-    </div>
+    <button style={commonStyle} onClick={onClick}>
+      {startIcon && <FontAwesomeIcon icon={startIcon} />}
+      {title}
+    </button>
   );
 }
